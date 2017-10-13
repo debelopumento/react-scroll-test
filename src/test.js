5bd1b796e3e3ef22c9e89d456a591d7acb6ac63b
debelopumento <imdiye@gmail.com>
2017-10-13 15:50:28 -0700

    react-scroll seems to work

--- src/test.js
+++ src/test.js
@@ -1,51 +1,110 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+'use strict'
 
-class Test extends Component {
-  test = event => {
-    window.scrollTo(0, 150)
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Scroll = require('react-scroll')
+
+var Link = Scroll.Link
+var DirectLink = Scroll.DirectLink
+var Element = Scroll.Element
+var Events = Scroll.Events
+var scroll = Scroll.animateScroll
+var scrollSpy = Scroll.scrollSpy
+
+var durationFn = function(deltaTop) {
+  return deltaTop
+}
+
+export default class Test extends React.Component {
+  constructor(props) {
+    super(props)
+    this.scrollToTop = this.scrollToTop.bind(this)
   }
 
+  componentDidMount() {
+    Events.scrollEvent.register('begin', function() {
+      console.log('begin', arguments)
+    })
+
+    Events.scrollEvent.register('end', function() {
+      console.log('end', arguments)
+    })
+
+    scrollSpy.update()
+  }
+  scrollToTop() {
+    scroll.scrollToTop()
+  }
+  componentWillUnmount() {
+    Events.scrollEvent.remove('begin')
+    Events.scrollEvent.remove('end')
+  }
   render() {
     return (
-      <div
-        style={{
-          //position: 'relative',
-          height: 400, //the button below only works if this line is commented out.
-          overflow: 'auto',
-        }}
-        id="testId"
-      >
-        <button onClick={this.test}>scroll to 150</button>
-        <p className="App-intro">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-          ea commodo consequat.
-        </p>
+      <div>
+        <div id="anchor" className="element">
+          test 6 (anchor)
+        </div>
+
+        <Link
+          activeClass="active"
+          to="firstInsideContainer"
+          spy={true}
+          smooth={true}
+          duration={250}
+          containerId="containerElement"
+          style={{ display: 'inline-block', margin: '20px' }}
+        >
+          Go to first element inside container
+        </Link>
+
+        <Link
+          activeClass="active"
+          to="secondInsideContainer"
+          spy={true}
+          smooth={true}
+          duration={250}
+          containerId="containerElement"
+          style={{ display: 'inline-block', margin: '20px' }}
+        >
+          Go to second element inside container
+        </Link>
+        <Element
+          name="test7"
+          className="element"
+          id="containerElement"
+          style={{
+            position: 'relative',
+            height: '200px',
+            overflow: 'scroll',
+            marginBottom: '100px',
+          }}
+        >
+          test 7 (duration and container)
+          <Element
+            name="firstInsideContainer"
+            style={{
+              marginBottom: '200px',
+            }}
+          >
+            first element inside container
+          </Element>
+          <Element
+            name="secondInsideContainer"
+            style={{
+              marginBottom: '200px',
+            }}
+          >
+            second element inside container
+          </Element>
+        </Element>
+
+        <Element id="same" className="element">
+          Two links point to this
+        </Element>
+
+        <a onClick={this.scrollToTop}>To the top!</a>
       </div>
     )
   }
 }
-
-export default Test
